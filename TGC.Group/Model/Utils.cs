4b1dc3b5c1c4df973769e386c99aba1c6e6da127
emitolaba95 <emi.tolaba95@gmail.com>
2017-04-05 15:04:54 -0300

    Detalle

--- TGC.Group/Model/Utils.cs
+++ TGC.Group/Model/Utils.cs
@@ -12,11 +12,23 @@ namespace TGC.Group.Model
     class Utils
     {
         /// <summary>
-        ///     Dispone un mesh en forma de circulo n veces dado un radio y el angulo
+        ///     Dispone un mesh en forma de circulo n veces dado un radio y el angulo. El angulo de fase es 0
         /// </summary>
         /// <param name="orginalMesh">Ruta donde esta la carpeta con los assets</param>
         /// <param name="lista">Lista del elemento a replicar. Debe estar instanciada</param>
         public static void disponerEnCirculoXZ(TgcMesh originalMesh, List<TgcMesh> lista, int veces, float radio, float angulo)
+        {
+            disponerEnCirculoXZ(originalMesh, lista, veces, radio, angulo, 0);
+        }
+
+
+        /// <summary>
+        ///     Dispone un mesh en forma de circulo n veces dado un radio, el angulo de fase y un angulo de desplazamiento.
+        /// </summary>
+        /// <param name="orginalMesh">Ruta donde esta la carpeta con los assets</param>
+        /// <param name="lista">Lista del elemento a replicar. Debe estar instanciada</param>
+        /// <param name="anguloFase">Angulo sobre el cual se comienza la disposicion</param>
+        public static void disponerEnCirculoXZ(TgcMesh originalMesh, List<TgcMesh> lista, int veces, float radio, float angulo, float anguloFase)
         {
             for (int i = 0; i < veces; i++)
             {
@@ -24,7 +36,10 @@ namespace TGC.Group.Model
                 var instance = originalMesh.createMeshInstance(originalMesh.Name + i);
                 instance.AutoTransformEnable = false;
 
-                instance.Transform = Matrix.Translation(radio * FastMath.Cos(i * angulo), 0, radio * FastMath.Sin(i * angulo)) * instance.Transform;
+                instance.Transform = Matrix.Translation(radio * FastMath.Cos((i * angulo) + anguloFase),
+                                                        0,
+                                                        radio * FastMath.Sin((i * angulo) + anguloFase))
+                                    * instance.Transform;
 
                 lista.Add(instance);
             }
